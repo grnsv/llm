2c1b7db1c890d92f70b2f0c41e1ea29d7a05bfc2
grnsv <grin.sv@gmail.com>
2024-09-27 21:11:02 +0300

    Use FastAPI

--- script.py
+++ script.py
@@ -1,25 +1,27 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-from flask import Flask, request, jsonify
+from fastapi import FastAPI
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-app = Flask(__name__)
+app = FastAPI()
 
-model_name = 'Vikhrmodels/it-5.4-fp16-orpo-v2'
+model_name = "Vikhrmodels/it-5.4-fp16-orpo-v2"
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     device_map="sequential",
     attn_implementation="sdpa",
-    torch_dtype=torch.bfloat16
+    torch_dtype=torch.bfloat16,
 )
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-@app.route('/', methods=['GET'])
-def generate_response():
-    prompt = request.args.get('q', default='Что ты такое?')
 
-    test_input = tokenizer.apply_chat_template([
-        {'role': 'user', 'content': prompt}
-    ], add_generation_prompt=True, tokenize=True, return_tensors='pt')
+@app.get("/")
+async def generate_response(prompt: str = "Что ты такое?"):
+    test_input = tokenizer.apply_chat_template(
+        [{"role": "user", "content": prompt}],
+        add_generation_prompt=True,
+        tokenize=True,
+        return_tensors="pt",
+    )
     test_input = test_input.to(model.device)
 
     answer = model.generate(
@@ -28,10 +30,13 @@ def generate_response():
         use_cache=True,
         max_new_tokens=256,
         temperature=0.3,
-    )[:, test_input.shape[-1]:]
+    )[:, test_input.shape[-1] :]
     answer = tokenizer.batch_decode(answer, skip_special_tokens=True)[0]
 
-    return jsonify({'response': answer})
+    return {"response": answer}
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8080)
