6c3421101747694f5a70dc0ba7847610f1aceed3
grnsv <grin.sv@gmail.com>
2024-09-28 01:25:50 +0300

    Added type hints

--- script.py
+++ script.py
@@ -1,22 +1,27 @@
 import torch
 from fastapi import FastAPI
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizerBase,
+)
 
-app = FastAPI()
+app: FastAPI = FastAPI()
 
-model_name = "Vikhrmodels/it-5.4-fp16-orpo-v2"
-model = AutoModelForCausalLM.from_pretrained(
+model_name: str = "Vikhrmodels/it-5.4-fp16-orpo-v2"
+model: PreTrainedModel = AutoModelForCausalLM.from_pretrained(
     model_name,
     device_map="sequential",
     attn_implementation="sdpa",
     torch_dtype=torch.bfloat16,
 )
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer: PreTrainedTokenizerBase = AutoTokenizer.from_pretrained(model_name)
 
 
 @app.get("/")
 async def generate_response(prompt: str = "Что ты такое?"):
-    test_input = tokenizer.apply_chat_template(
+    test_input: torch.Tensor = tokenizer.apply_chat_template(
         [{"role": "user", "content": prompt}],
         add_generation_prompt=True,
         tokenize=True,
@@ -24,14 +29,14 @@ async def generate_response(prompt: str = "Что ты такое?"):
     )
     test_input = test_input.to(model.device)
 
-    answer = model.generate(
+    answer: torch.Tensor = model.generate(
         test_input,
         do_sample=True,
         use_cache=True,
         max_new_tokens=256,
         temperature=0.3,
     )[:, test_input.shape[-1] :]
-    answer = tokenizer.batch_decode(answer, skip_special_tokens=True)[0]
+    answer: str = tokenizer.batch_decode(answer, skip_special_tokens=True)[0]
 
     return {"response": answer}
 
